0445cb1f9d050259a5e8808700f0281ec0aa875b
ditsky <34258091+ditsky@users.noreply.github.com>
2018-07-03 11:51:59 -0400

    express working

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+
+    'googleAuth' : {
+        'clientID': '16004077352-fng0mdvc2unic9gq9g5t7k0tec3nfr5m.apps.googleusercontent.com',
+        'clientSecret'  : '2Q_vlGhK_GLiu8IyCHoxShI0',
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:5400/'
+    }
+
+};
