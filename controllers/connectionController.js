7759076aa24f465b7d104f41c3917b99ff670698
ditsky <34258091+ditsky@users.noreply.github.com>
2018-07-23 22:51:33 -0400

    remove useless

--- controllers/connectionController.js
+++ controllers/connectionController.js
@@ -2,23 +2,6 @@
 const Connection = require('../models/connection');
 const axios = require('axios');
 
-// this displays all of the skills
-exports.render = (req, res) => {
-  //console.log('in getAllLinks');
-  Link.find({})
-    .exec()
-    .then(links => {
-      res.render('connection', { links: links });
-    })
-    .catch(error => {
-      console.log(error.message);
-      return [];
-    })
-    .then(() => {
-      console.log('link promise complete');
-    });
-};
-
 
 exports.sendUserData = (req, res) => {
   let newConnection = new Connection ({
